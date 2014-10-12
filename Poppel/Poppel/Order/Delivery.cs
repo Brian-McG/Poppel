5721c4df93aad232ef5380e31ed058dc525a61e4
Brian Mc George <mcgbri004@myuct.ac.za>
2014-10-12 20:30:46 +0200

    Add missing delivery class.

---
+++ Poppel/Poppel/Order/Delivery.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poppel.Order
+{
+    public class Delivery
+    {
+        private DateTime startDeliveryTime;
+        private DateTime endDeliveryTime;
+        private Collection<DateTime> allowedDeliveryDates;
+
+        public DateTime StartDeliveryTime
+        {
+            get { return startDeliveryTime; }
+            set { startDeliveryTime = value; }
+        }
+        public DateTime EndDeliveryTime
+        {
+            get { return endDeliveryTime; }
+            set { endDeliveryTime = value; }
+        }
+        public Collection<DateTime> AllowedDeliveryDates
+        {
+            get { return allowedDeliveryDates; }
+            set { allowedDeliveryDates = value; }
+        }
+    }
+}
